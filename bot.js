c4d3ba99999e9d4558ed6e6127c0f6ad1539675a
R3dAlArb <45834562+R3dAlArb@users.noreply.github.com>
2019-06-17 15:46:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,41 +4,16 @@ const fs = require("fs");
 var prefix = "$"
 
 
-const devs = ["349607341320306690"];
-const adminprefix = ["$"];
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
- 
-  if (message.content.startsWith(adminprefix + 'pl')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
-  } else
-     if (message.content === (adminprefix + "lev")) {
-    message.guild.leave();
-  } else
-  if (message.content.startsWith(adminprefix + 'w')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
-  } else
-  if (message.content.startsWith(adminprefix + 'l')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
-  } else
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/idk");
-      message.channel.send(`**✅ تم تغيير الحالة بنجاح **`)
-  }
-  if (message.content.startsWith(adminprefix + 'name')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`**✅ تم تغير الاسم بنجاح ${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'av')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`**✅ تم تغير الصور بنجاح ${argresult}** `);
-}
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : iiLord_YT...?? ..#0061 `);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : iiLord_YT...?? ..#0061  ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`--___--`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
 });
-
 
 
 
